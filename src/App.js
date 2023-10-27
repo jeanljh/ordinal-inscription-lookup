76066b8cc99f3c6ef7ad93e7e76abaee7458ed51
jean <leejeanhwa@gmail.com>
2023-10-27 10:52:05 +0800

    minor code refactor

--- src/App.js
+++ src/App.js
@@ -51,23 +51,21 @@ const App = () => {
       <button onClick={handleLookup}>Look up</button>
       <h2>Results</h2>
       <div>
-        {searching ? (
-          <div className='status'>Searching...</div>
-        ) : (
-          searchPerformed && results.length > 0 && (
-            results.map((inscriptionId, index) => (
-              <div
-                className={`group ${clicked === index ? 'clicked' : ''}`}
-                key={index}
-                onClick={() => handleClick(index)}
-              >
-                Inscription {inscriptionId.slice(0, 8)}
-                {clicked === index && <span> Clicked</span>}
-              </div>
-            ))
+        {
+          searching ? <div className='status'>Searching...</div> : 
+          searchPerformed && results.length > 0 && results.map((inscriptionId, index) =>
+            <div
+              className={`group ${clicked === index ? 'clicked' : ''}`}
+              key={index}
+              onClick={() => handleClick(index)}
+            > Inscription {inscriptionId.slice(0, 8)} {clicked === index && <span>Clicked</span>}
+            </div>
           )
-        )}
-        {searchPerformed && results.length < 1 && !searching && <div className='status'>No Inscription IDs found</div>}
+        }
+        {
+          searchPerformed && results.length < 1 && !searching && 
+          <div className='status'>No Inscription IDs found</div>
+        }
       </div>
     </div>
   );
